e4bf3854ab551c80b81574dc9989ff7d4016d98b
marijnschilling <marijnschilling@gmail.com>
2018-12-04 12:14:39 +0100

    Create project

---
+++ Popcorn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Popcorn
+//
+//  Created by Marijn Schilling on 04/12/2018.
+//  Copyright © 2018 Marijn Schilling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
