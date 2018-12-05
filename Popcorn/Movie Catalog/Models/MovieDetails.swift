e4ff76310f6d4e7b8a91e702deecdceeb915b4c3
marijnschilling <marijnschilling@gmail.com>
2018-12-05 11:05:08 +0100

    Format date in custom init from decoder

--- Popcorn/Movie Catalog/Models/MovieDetails.swift
+++ Popcorn/Movie Catalog/Models/MovieDetails.swift
@@ -10,6 +10,7 @@ struct MovieDetails: Codable {
     let tagline: String
     let overview: String
     let voteAverage: Double
+    let releaseDate: Date
     let posterPath: String
     let backdropPath: String
 
@@ -18,7 +19,28 @@ struct MovieDetails: Codable {
         case tagline
         case overview
         case voteAverage = "vote_average"
+        case releaseDate = "release_date"
         case posterPath = "poster_path"
         case backdropPath = "backdrop_path"
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        title = try container.decode(String.self, forKey: .title)
+        tagline = try container.decode(String.self, forKey: .tagline)
+        overview = try container.decode(String.self, forKey: .overview)
+        voteAverage = try container.decode(Double.self, forKey: .voteAverage)
+        posterPath = try container.decode(String.self, forKey: .posterPath)
+        backdropPath = try container.decode(String.self, forKey: .backdropPath)
+
+        let dateString = try container.decode(String.self, forKey: .releaseDate)
+        let formatter = DateFormatter.yearMonthDay
+        if let date = formatter.date(from: dateString) {
+            releaseDate = date
+        } else {
+            throw DecodingError.dataCorruptedError(forKey: .releaseDate,
+                    in: container,
+                    debugDescription: "Date string does not match format expected by formatter.")
+        }
     }
 }
--- PopcornTests/MovieDetailsTests.swift
+++ PopcornTests/MovieDetailsTests.swift
@@ -38,6 +38,7 @@ class MovieDetailsTests: XCTestCase {
             XCTAssertEqual(movieDetails.tagline, "Fate of One. Future of All.")
             XCTAssertEqual(movieDetails.overview, "Gellert Grindelwald has escaped imprisonment and has begun gathering followers to his cause—elevating wizards above all non-magical beings. The only one capable of putting a stop to him is the wizard he once called his closest friend, Albus Dumbledore. However, Dumbledore will need to seek help from the wizard who had thwarted Grindelwald once before, his former student Newt Scamander, who agrees to help, unaware of the dangers that lie ahead. Lines are drawn as love and loyalty are tested, even among the truest friends and family, in an increasingly divided wizarding world.")
             XCTAssertEqual(movieDetails.voteAverage, 7.4)
+            XCTAssertEqual(movieDetails.releaseDate, testDate)
             XCTAssertEqual(movieDetails.posterPath, "/uyJgTzAsp3Za2TaPiZt2yaKYRIR.jpg")
             XCTAssertEqual(movieDetails.backdropPath, "/xgbeBCjmFpRYHDF7tQ7U98EREWp.jpg")
         } catch {
