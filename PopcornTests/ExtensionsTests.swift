b47dd4f84095309897db101a38bb4a581999fc1f
marijnschilling <marijnschilling@gmail.com>
2018-12-05 12:50:57 +0100

    Add tiny constraints pod

--- PopcornTests/ExtensionsTests.swift
+++ PopcornTests/ExtensionsTests.swift
@@ -27,4 +27,9 @@ class ExtensionsTest: XCTestCase {
         let youTubeURL = URL.fetchTrailerURL(forIdentifier: 12344)
         XCTAssertEqual(youTubeURL?.absoluteString, "https://api.themoviedb.org/3/movie/12344/videos?api_key=3b7ad5dde84acc0a427e3fd01285a3c1&language=en-US")
     }
+
+    func testPosterURL() {
+        let youTubeURL = URL.fetchPosterURL(forPosterPath: "hahaha.jpg")
+        XCTAssertEqual(youTubeURL?.absoluteString, "https://image.tmdb.org/t/p/w300/hahaha.jpg")
+    }
 }
